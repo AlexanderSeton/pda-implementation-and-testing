df88bea11ce1990a10bd0847da66b71aa5375d7a
Alexander Seton <alexanderseton@protonmail.com>
2021-11-15 17:46:23 +0000

    Task 1 added some more errors.

--- part_2_code/specs/card_game_tests.py
+++ part_2_code/specs/card_game_tests.py
@@ -2,5 +2,18 @@ import unittest
 from src.card import Card
 from src.card_game import CardGame
 
-class TestCardGame:
-    pass
+class TestCardGame(unittest.TestCase):
+    def setUp(self):
+        self.cardgame1 = CardGame()
+        self.card1 = Card("Spades", 1)
+        self.card2 = Card("Hearts", 5)
+    
+    def test_cardgame_check_for_ace(self):
+        self.assertEqual(True, self.cardgame1.check_for_ace(self.card1))
+    
+    def test_cardgame_return_highest_card(self):
+        self.assertEqual(self.card1, self.cardgame1.highest_card(self.card1, self.card2))
+    
+    def test_cardgame_total_cards(self):
+        cards = [self.card1, self.card2]
+        self.assertEqual("You have a total of 2", self.cardgame1.cards_total(cards))
--- part_2_code/src/card_game.py
+++ part_2_code/src/card_game.py
@@ -8,25 +8,29 @@ class CardGame:
 
 
   def check_for_ace(self, card):
-    if card.value = 1:
+    if card.value == 1:
       return True
-    else
+    else:
       return False
    
 
-  dif highest_card(self, card1 card2):
-  if card1.value > card2.value:
-    return card
-  else:
-    return card2
+  def highest_card(self, card1, card2):
+    if self.check_for_ace(card1):
+      return card1
+    elif self.check_for_ace(card2):
+      return card2
+    elif card1.value > card2.value:
+      return card1
+    else:
+      return card2
   
 
 
-def cards_total(self, cards):
-  total
-  for card in cards:
-    total += card.value
-    return "You have a total of" + total
+  def cards_total(self, cards):
+    total = 0
+    for card in cards:
+      total += 1
+    return "You have a total of " + str(total)
 
 
 # Task 1: Static Testing
@@ -37,3 +41,12 @@ def cards_total(self, cards):
   # line 18: if is not indented
   # line 19: should return card1
   # line 26: total should be assigned to 0
+  # line 28: total should be incremented by 1 for every card, not by the cards value
+  # line 29: return needs to be outdented
+  # line 29: must make the int total a string 
+  # line 25 - line 29: whole function needs to be indented
+
+
+
+
+
